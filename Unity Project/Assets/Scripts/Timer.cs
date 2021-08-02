bdbe7446f246fb485a5cdcf41757b3ed9a17e4fd
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-02 23:25:31 +0300

    CreateTimer

---
+++ Unity Project/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timer;
+
+    private float time;
+    private float msec;
+    private float sec;
+    private float min;
+
+
+    public void StartTimer()
+    {
+        StartCoroutine(TimerProcessing());
+    }    
+    
+    public void StopTimer()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator TimerProcessing()
+    {
+        for (; ; )
+        {
+            time += Time.deltaTime;
+            msec = (int)((time - (int)time) * 100);
+            sec = (int)(time % 60);
+            min = (int)(time / 60 % 60);
+
+            timer.text = string.Format("{0:00}:{1:00}:{2:00}", min, sec, msec);
+            yield return null;
+        }
+    }
+}
