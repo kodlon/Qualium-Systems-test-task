bb9694248bb6968a3e5e82c5cb61dac2a3dc3733
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-03 00:52:29 +0300

    Add calibration

--- Unity Project/Assets/Scripts/StartGame.cs
+++ Unity Project/Assets/Scripts/StartGame.cs
@@ -4,12 +4,14 @@ public class StartGame : MonoBehaviour
 {
     [SerializeField] Timer timer;
     [SerializeField] Rigidbody ballBody;
+    [SerializeField] PlatformControl platform;
 
     public static bool GameActive = false;
 
     public void StartButtonPressed()
     {
         GameActive = true;
+        platform.CalibrateAccelerometer();
         GameStarted();
     }
 
