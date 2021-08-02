b972fb86c0dd280dd7aee510f314f1c8b974dcdf
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-02 20:21:12 +0300

    first try controll

---
+++ Unity Project/Assets/Scripts/PlatformControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlatformControl : MonoBehaviour
+{
+    private const float SPEED_ROTATE = 150;
+
+    float xRotation = 0;
+    float yRotation = 0;
+
+
+    void FixedUpdate()
+    {
+        MoveAcceleration();
+    }
+
+    private void MoveAcceleration()
+    {
+        float xAxis = Input.acceleration.x;
+        float yAxis = Input.acceleration.y;
+
+        yRotation += (yAxis * SPEED_ROTATE) * Time.fixedDeltaTime;
+        yRotation = Mathf.Clamp(yRotation, -30f, 30f);
+
+        xRotation += (xAxis * SPEED_ROTATE) * Time.fixedDeltaTime;
+        xRotation = Mathf.Clamp(xRotation, -30f, 30f);
+
+        transform.rotation = Quaternion.Euler(yRotation, 0, -xRotation);
+    }
+}
