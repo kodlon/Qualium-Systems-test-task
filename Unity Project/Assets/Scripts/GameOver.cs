f5b766d6ce9b640253ce3c5d6a5e2e8981519bef
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-02 23:25:06 +0300

    Create GameOver script

---
+++ Unity Project/Assets/Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] Timer timer;
+    [SerializeField] Canvas gameOverMenu;
+    [SerializeField] Rigidbody ballBody;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameOverMenu.gameObject.SetActive(true);
+        ballBody.useGravity = false;
+        ballBody.velocity = Vector3.zero;
+        timer.StopTimer();
+    }
+
+    public void RestartButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
