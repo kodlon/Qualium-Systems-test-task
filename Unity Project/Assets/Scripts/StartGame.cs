15c54b12c1c9ae0d2ed3846f562edbdf6cb85350
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-02 23:25:21 +0300

    Create StartGame script

---
+++ Unity Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] Timer timer;
+    [SerializeField] Rigidbody ballBody;
+
+    public static bool GameActive = false;
+
+    public void StartButtonPressed()
+    {
+        GameActive = true;
+        GameStarted();
+    }
+
+    private void GameStarted()
+    {
+        this.gameObject.SetActive(false);
+        ballBody.useGravity = true;
+        timer.StartTimer();
+    }
+}
